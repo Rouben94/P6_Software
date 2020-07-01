8dca06327e219e930444dda7a1a8031b8a73c49c
Rouben94 <bobst.robin@ggs.ch>
2020-07-01 11:41:32 +0200

    Added CLI functionality
    Read out the results from the benchmark test with cli

--- Openthread/Python/python-cli-interface.py
+++ Openthread/Python/python-cli-interface.py
@@ -1,24 +1,42 @@
-import serial, time
+import serial, multiprocessing, time, re
 
-serialPort = serial.Serial(port="/dev/ttyACM0", baudrate=115200, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
-
+serialPort = serial.Serial(port="COM9", baudrate=115200, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
 serialString = ""
 
-# Open the srial port ttyACM0
-serialPort.close()
+bm_message_list = []
+bm_time_list = []
 
-# Send state command to the master node
-serialPort.write(b"benchmark_start 1000")
-serialPort.write(b"\r\n")
+def bm_start(time):
+    serial_command = "benchmark_start {}\n"
+    serialPort.write(bytes(serial_command.format(time).encode('Ascii')))
+
+bm_time = float(input("Benchmark Zeit:"))
+close_time = time.time() + bm_time
 
 while(1):
-    # Wait until there is data waiting in the serial buffer
-    if(serialPort.in_waiting > 0):
+    close_time2 = time.time() + 12.0
+    bm_start(10000)
 
-        # Read data out of the buffer until a carraige return / new line is found
-        serialString = serialPort.readline()
+    while(1):
+        # Wait until there is data waiting in the serial buffer
+        if(serialPort.in_waiting > 0):
+            # Read data out of the buffer until a carraige return / new line is found
+            serialString = serialPort.readline().decode('Ascii')
 
-        # Print the contents of the serial data
-        print(serialString.decode('Ascii'))
-
+            # Print the contents of the serial data
+            if "ID" in serialString:
+                result = re.findall(r"\d+", serialString)
+                if result[0] in bm_message_list:
+                    bm_time_list.append(abs(int(result[1])-int(bm_message_list[bm_message_list.index(result[0])+1])))
+                else:
+                    bm_message_list.append(result[0])
+                    bm_message_list.append(result[1])
+        
+        if time.time()>close_time2:
+            bm_message_list.clear()
+            print(bm_time_list)
+            break
+    
+    if time.time()>close_time:
+        bm_time_list.clear
         break
