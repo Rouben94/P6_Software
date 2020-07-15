4f6fbf7719bffa2f8fe34e46ba2b22eaf153c380
hakkimakki <raffael.anklin@gmail.com>
2020-07-15 11:26:39 +0200

    Working

--- Bluetooth/BSN/src/main.cpp
+++ Bluetooth/BSN/src/main.cpp
@@ -1,11 +1,12 @@
 #include <zephyr.h>
 
+/*
 //#include "bm_blemesh.h"
 #include "bm_timesync.h"
 #include "bm_cli.h"
 #include "bm_config.h"
 #include "bm_statemachine.h"
-
+*/
 #include "Simple_nrf_radio.hpp"
 
 Simple_nrf_radio simple_radio;
@@ -13,10 +14,11 @@ Simple_nrf_radio simple_radio;
 
 void main(void)
 {
-	bm_cli_log("Benchmark Started\n");
+	printk("Benchmark Started\n");
+	k_sleep(K_MSEC(1000));
 	//bm_statemachine();
-	if (isTimeMaster){
-		char *Test_String = "C Programming is the shit";
+	if (false){
+		char *Test_String = "C Programming is the shit\0";
 		RADIO_PACKET s = {};
 		s.PDU = (u8_t *)Test_String;
 		s.length = strlen(Test_String) + 1; // Immer Länge + 1 bei String
@@ -26,8 +28,11 @@ void main(void)
 	} else {
 		RADIO_PACKET r = {};
 		simple_radio.Receive(&r,K_FOREVER);
-		printk("%s",r.PDU);
+		printk("%s",(char *)r.PDU);
+		k_sleep(K_MSEC(10));
 		k_sleep(K_FOREVER);		
 	}
 	//bm_blemesh_enable();
 }
+
+	
