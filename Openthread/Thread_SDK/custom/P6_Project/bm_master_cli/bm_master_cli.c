5b82e8194a09b78dc056a301115747c396a1d86e
Rouben94 <bobst.robin@ggs.ch>
2020-07-16 09:29:51 +0200

    fixed IPv6 to string bug

--- Openthread/Thread_SDK/custom/P6_Project/bm_master_cli/bm_master_cli.c
+++ Openthread/Thread_SDK/custom/P6_Project/bm_master_cli/bm_master_cli.c
@@ -21,42 +21,13 @@ void bm_cli_write_result(bm_message_info message_info)
     char buf7[50] = {"\0"};
     char buf8[50] = {"\0"};
 
-    for(int i=0; i<8; i++)
-    {
-        char buf9[5] = {"\0"};
-        char buf10[5] = {"\0"};
-
-        if(i==7)
-        {
-            sprintf(buf9, "%x", message_info.source_address.mFields.m16[i]);
-            sprintf(buf10, "%x", message_info.dest_address.mFields.m16[i]);
-        } else
-        {
-            sprintf(buf9, "%x:", message_info.source_address.mFields.m16[i]);
-            sprintf(buf10, "%x:", message_info.dest_address.mFields.m16[i]);
-        }
-
-        if(message_info.source_address.mFields.m16[i] == 0)
-        {
-            sprintf(buf9, ":", message_info.source_address.mFields.m16[i]);
-        }
-
-        if(message_info.dest_address.mFields.m16[i] == 0)
-        {
-            sprintf(buf10, ":", message_info.dest_address.mFields.m16[i]);
-        }
-
-        strncat(buf6, buf9, 5);
-        strncat(buf7, buf10, 5);
-    }
-
     sprintf(buf1, "ID: %u, ", message_info.message_id);
     sprintf(buf2, "Time: %u us, ", message_info.net_time);
     sprintf(buf3, "Ack_Time: %u us, ", message_info.net_time_ack);
     sprintf(buf4, "Hops: %u, ", message_info.number_of_hops);
     sprintf(buf5, "RSSI: %d dB, ", message_info.RSSI);
-    sprintf(buf6, "Source: %s, ", buf6);
-    sprintf(buf7, "Dest: %s, ", buf7);
+    sprintf(buf6, "Src: %x:%x:%x:%x:%x:%x:%x:%x, ", message_info.source_address.mFields.m16[0], message_info.source_address.mFields.m16[1], message_info.source_address.mFields.m16[2], message_info.source_address.mFields.m16[3], message_info.source_address.mFields.m16[4], message_info.source_address.mFields.m16[5], message_info.source_address.mFields.m16[6], message_info.source_address.mFields.m16[7]);
+    sprintf(buf7, "Dst: %x:%x:%x:%x:%x:%x:%x:%x, ", message_info.dest_address.mFields.m16[0], message_info.dest_address.mFields.m16[1], message_info.dest_address.mFields.m16[2], message_info.dest_address.mFields.m16[3], message_info.dest_address.mFields.m16[4], message_info.dest_address.mFields.m16[5], message_info.dest_address.mFields.m16[6], message_info.dest_address.mFields.m16[7]);
 
     if (message_info.data_size)
     {
@@ -71,7 +42,7 @@ void bm_cli_write_result(bm_message_info message_info)
     otCliOutput(buf3, sizeof(buf3));
     otCliOutput(buf4, sizeof(buf4));
     otCliOutput(buf5, sizeof(buf5));
-    otCliOutput(buf6, sizeof(buf3));
-    otCliOutput(buf7, sizeof(buf4));
-    otCliOutput(buf8, sizeof(buf5));
+    otCliOutput(buf6, sizeof(buf6));
+    otCliOutput(buf7, sizeof(buf7));
+    otCliOutput(buf8, sizeof(buf8));
 }
