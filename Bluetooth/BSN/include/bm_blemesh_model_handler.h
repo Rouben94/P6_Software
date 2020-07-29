48d5a92af7064ee3b83e4b39ddd3b323503ecb4b
hakkimakki <raffael.anklin@gmail.com>
2020-07-29 16:22:29 +0200

    Added GPL Header

--- Bluetooth/BSN/include/bm_blemesh_model_handler.h
+++ Bluetooth/BSN/include/bm_blemesh_model_handler.h
@@ -1,13 +1,21 @@
 /*
- * Copyright (c) 2019 Nordic Semiconductor ASA
- *
- * SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
- */
+This file is part of Bluetooth-Benchamrk.
 
-/**
- * @file
- * @brief Model handler
- */
+Bluetooth-Benchamrk is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Bluetooth-Benchamrk is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Bluetooth-Benchamrk.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/* AUTHOR 	 :    Raffael Anklin       */
 
 #ifndef MODEL_HANDLER_H__
 #define MODEL_HANDLER_H__
--- Bluetooth/BSN/src/bm_blemesh.c
+++ Bluetooth/BSN/src/bm_blemesh.c
@@ -1,12 +1,23 @@
 /*
- * Copyright (c) 2019 Nordic Semiconductor ASA
- *
- * SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
- */
+This file is part of Bluetooth-Benchamrk.
 
-/** @file
- *  @brief Nordic Mesh light sample
- */
+Bluetooth-Benchamrk is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Bluetooth-Benchamrk is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Bluetooth-Benchamrk.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/* AUTHOR 	 :    Raffael Anklin       */
+
+
 #include "bm_config.h"
 #include <bluetooth/bluetooth.h>
 #include <bluetooth/mesh/models.h>
