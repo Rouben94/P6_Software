4df227966d3a4950eca277937f7fe7a8e6cecf81
Rouben94 <bobst.robin@ggs.ch>
2020-08-11 14:39:43 +0200

    changed zip files

--- Benchmark Management/ot_benchmark.py
+++ Benchmark Management/ot_benchmark.py
@@ -16,9 +16,9 @@ bm_result_list = []
 stop_threads = False
 csv_title = ("ot_bm_result " + datetime.today().strftime('%H.%M %d-%m-%Y'))
 
-def bm_start(time):
-    serial_command = "benchmark_start {}\n"
-    serial_port.write(bytes(serial_command.format(time).encode('Ascii')))
+def bm_start(time, messages, payload, mode, clients):
+    serial_command = "benchmark_start {} {} {} {} {}\n"
+    serial_port.write(bytes(serial_command.format(time, messages, payload, mode, clients).encode('Ascii')))
 
 def read():
     global stop_threads
@@ -32,41 +32,27 @@ def read():
                 if ">" in bm_result: bm_result.remove(">")
                 if "<" in bm_result: bm_result.remove("<")
                 bm_result[0] = bm_result[5] + '_' + bm_result[0]
-                if len(bm_result) == 9: bm_result_list.append(bm_result) 
+                if len(bm_result) == 10: bm_result_list.append(bm_result) 
                 print(bm_result)
 
-            elif "<REPORT_END>" in serialString:
-                sleep(1)
-                if stop_threads:
-                    df = pd.DataFrame(bm_result_list, columns = ['Message ID', 'Timestamp (us)','Ack Timestamp (us)', 'Hops','RSSI','Source Address','Destination Address','Group Address','Data Size'])
-                    path = dirpath+'\\result_files\\'+csv_title+'.csv'
-                    df.to_csv(path,index=False,sep=';',encoding='utf-8')
-                    break
-                bm_start(30000)
-
-""" def write():
-    global stop_threads
-    input("Start with enter: ")
-    while(1):
-        bm_start(60000)
-        sleep(90)
         if stop_threads:
-            df = pd.DataFrame(bm_result_list, columns = ['Message ID', 'Timestamp (us)','Ack Timestamp (us)', 'Hops','RSSI','Source Address','Destination Address','Group Address','Data Size'])
+            df = pd.DataFrame(bm_result_list, columns = ['Message ID', 'Timestamp (us)','Ack Timestamp (us)', 'Hops','RSSI','Source Address','Destination Address','Group Address','Data Size', 'Node_ID'])
             path = dirpath+'\\result_files\\'+csv_title+'.csv'
             df.to_csv(path,index=False,sep=';',encoding='utf-8')
-            break """
+            break
 
 if __name__ == "__main__":
     t_read = threading.Thread(target=read)
-    """ t_write = threading.Thread(target=write) """
     
     if serial_port.isOpen(): serial_port.close()
     serial_port.open()
 
     t_read.start()
-    """ t_write.start() """
 
-    bm_time = input("How long would you like to test? (min): ")
-    bm_start(30000)
-    sleep(int(bm_time)*60)
-    stop_threads = True
+    bm_parameters = input("Please insert Parameters <Benchmark time in s> <Number of messages> <Payload in Byte> <mode> <Number of Clients>").split()
+    bm_start(bm_parameters[0], bm_parameters[1], bm_parameters[2], bm_parameters[3], bm_parameters[4])
+    sleep(int(bm_parameters[0])+70)
+    while(True):
+        if input("Did you got all results?") == 'y':
+            stop_threads = True
+            break
