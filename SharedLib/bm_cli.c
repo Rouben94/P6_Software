247ea6d7e8c87d677111c66d0f32cef297794069
honeybadgerstuff <47782168+honeybadgerstuff@users.noreply.github.com>
2020-08-12 14:13:03 +0200

    Some cosmetics

--- SharedLib/bm_cli.c
+++ SharedLib/bm_cli.c
@@ -45,7 +45,6 @@ along with Benchamrk-Shared-Library.  If not, see <http://www.gnu.org/licenses/>
 #include "app_error.h"
 #include "app_timer.h"
 #include "app_util.h"
-//#include "fds.h"
 
 #include "nrf_cli.h"
 #include "nrf_cli_rtt.h"
@@ -65,8 +64,8 @@ along with Benchamrk-Shared-Library.  If not, see <http://www.gnu.org/licenses/>
 #endif
 
 /* Init the Parameters */
-bm_params_t bm_params = {BENCHMARK_DEFAULT_TIME_S, BENCHMARK_DEFAULT_PACKETS_CNT,0,0,0,0,0,0,0};
-bm_params_t bm_params_buf = {BENCHMARK_DEFAULT_TIME_S, BENCHMARK_DEFAULT_PACKETS_CNT,0,0,0,0,0,0,0};
+bm_params_t bm_params = {BENCHMARK_DEFAULT_TIME_S, BENCHMARK_DEFAULT_PACKETS_CNT, 0, 0, 0, 0, 0, 0, 0};
+bm_params_t bm_params_buf = {BENCHMARK_DEFAULT_TIME_S, BENCHMARK_DEFAULT_PACKETS_CNT, 0, 0, 0, 0, 0, 0, 0};
 
 #ifdef ZEPHYR_BLE_MESH
 void bm_cli_log(const char *fmt, ...) {
@@ -122,7 +121,6 @@ static void cli_init(void) {
 void bm_cli_process(void) {
 #ifdef BENCHMARK_MASTER
   nrf_cli_process(&m_cli_libuarte);
-//  nrf_cli_process(&m_cli_uart);
 #endif
 }
 void bm_cli_init(void) {
@@ -133,15 +131,6 @@ void bm_cli_init(void) {
   APP_ERROR_CHECK(ret);
   nrf_drv_clock_lfclk_request(NULL);
 
-  //  ret = app_timer_init();
-  //  APP_ERROR_CHECK(ret);
-  //
-  //  ret = app_timer_create(&m_timer_0, APP_TIMER_MODE_REPEATED, timer_handle);
-  //  APP_ERROR_CHECK(ret);
-  //
-  //  ret = app_timer_start(m_timer_0, APP_TIMER_TICKS(1000), NULL);
-  //  APP_ERROR_CHECK(ret);
-
   cli_init();
   cli_start();
 
@@ -150,11 +139,8 @@ void bm_cli_init(void) {
 }
 
 void bm_cli_log_init(void) {
-  //#ifdef BENCHMARK_MASTER
-  //  APP_ERROR_CHECK(NRF_LOG_INIT(app_timer_cnt_get));
-  //#else
+
   APP_ERROR_CHECK(NRF_LOG_INIT(NULL));
-  //#endif
   NRF_LOG_DEFAULT_BACKENDS_INIT();
 }
 #endif
